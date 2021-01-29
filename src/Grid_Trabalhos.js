91b93b1962cfaca10e82ef20172ce2156aeb1200
DESKTOP-A5C3HCP\Tiago <tiaciole@gmail.com>
2021-01-29 19:02:34 -0300

    grid trabalhos

--- src/Grid_Trabalhos.js
+++ src/Grid_Trabalhos.js
@@ -4,28 +4,28 @@ import './App.css'
 
 function Grid_Trabalhos(){
     return(
-        <div>
-            <div>
-                1
+        <div className = "grid-trabalhos">
+            <div className = 'trabalho1'>
+                
             </div>
 
-            <div>
+            <div className = 'trabalho2'>
                 2
             </div>
 
-            <div>
+            <div className = 'trabalho3'>
                 3
             </div>
 
-            <div>
+            <div className = 'trabalho4'>
                 4
             </div>
 
-            <div>
+            <div className = 'trabalho5'>
                 5
             </div>
 
-            <div>
+            <div className = 'trabalho6'>
                 6
             </div>
 
